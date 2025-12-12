41897d236b9677f7c01c7a64e3532e5d19401dea
Jess-Home <yabotuandui@gmail.com>
2025-12-12 23:49:57 +0800

    增加客服文件

---
+++ 内嵌接口/index.php
@@ -0,0 +1,14 @@
+<?php 
+$TEXT =  '{
+    "links": {
+        "shopCenter": "https://tkweb.tiktok-169vip.xyz/code?path=/home&", 
+        "goodsList": "https://tkweb.tiktok-169vip.xyz/code?path=/product&",
+        "orderList": "https://tkweb.tiktok-169vip.xyz/code?path=/order&"
+    }
+}';
+//加密
+$encoded_string = base64_encode($TEXT);
+echo $encoded_string;
+
+
+?>
---
+++ 客服/service/config.php
@@ -0,0 +1,19 @@
+<?php
+// 这里填写客服系统域名，前面带http://，用于pusher系统通知客服平台客户或者客服上下线
+$domain = 'http://tiktok-kf658vip.xyz';
+
+// App_key，客服系统与pusher通讯的key
+$app_key = 'zgxn5ohczieswnnf';
+
+// App_secret，客服系统与pusher通讯的密钥
+$app_secret = 'd1slqp965yshx75bxg4rgtvklet5gszj';
+
+// App id
+$app_id = 232;
+
+// websocket 端口，客服系统网页会连这个端口
+$websocket_port = 456;
+
+// Api 端口，用于后端与pusher通讯
+$api_port = 2080;
+
